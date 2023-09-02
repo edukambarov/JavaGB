ebdc91c05a15dd0517d46ddb4a8e5da28ff7889d
edukambarov <edukambarovgb@yandex.ru>
2023-09-02 12:26:35 +0300

    lect1

---
+++ Java_lection1/src/main/java/Program.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+//        Scanner iScanner = new Scanner(System.in);
+//        System.out.printf("name: ");
+//        String name = iScanner.nextLine();
+//        System.out.printf("Привет, %s!", name);
+//        iScanner.close();
+
+//        String s = "qwe";
+//        int a= 123;
+//        String q = s + a;
+//        System.out.println(q);
+
+//        int a = 1, b = 2;
+//        int c = a + b;
+//        String res = String.format("%d + %d = %d \n", a, b, c);
+//        System.out.printf("%d + %d = %d \n", a, b, c);
+//        System.out.println(res);
+
+//        int b = 111;
+//        {
+//            int a = 222;
+//            System.out.println(a+b);
+//        }
+//        int a = 132;
+//        System.out.println(a);
+
+
+
+
+
+
+
+
+
+
+    }
+    static String getType(Object o) {
+        return o.getClass().getSimpleName();
+
+    }
+}
